b7a535d516b4d427ecbcd6f9efe306a05459c33d
Sam Odom <samodom@me.com>
2014-10-12 22:01:23 -0500

    Initial Commit

---
+++ FoundationSwagger/FoundationSwagger.h
@@ -0,0 +1,19 @@
+//
+//  FoundationSwagger.h
+//  FoundationSwagger
+//
+//  Created by Sam Odom on 10/12/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoundationSwagger.
+FOUNDATION_EXPORT double FoundationSwaggerVersionNumber;
+
+//! Project version string for FoundationSwagger.
+FOUNDATION_EXPORT const unsigned char FoundationSwaggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoundationSwagger/PublicHeader.h>
+
+
